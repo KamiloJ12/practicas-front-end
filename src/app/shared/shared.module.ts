c5a68c5c5454d4e92d7ab2c05a76b1efba603a9a
Camilo Jaimes <Camilo12052000@hotmail.com>
2023-10-14 18:26:19 -0500

    crear logo del aplicativo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorLabelDirective } from './directives/error-label.directive';
+import { LogoComponent } from './components/logo/logo.component';
 
 
 @NgModule({
   declarations: [
-    ErrorLabelDirective
+    ErrorLabelDirective,
+    LogoComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ErrorLabelDirective,
+    LogoComponent
   ]
 })
 export class SharedModule { }
