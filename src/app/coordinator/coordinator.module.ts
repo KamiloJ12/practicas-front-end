e1c13209880888e691ca147d66b3fc6a177ffcb6
Camilo Jaimes <Camilo12052000@hotmail.com>
2023-10-20 08:04:34 -0500

    mover componente sidebar a modulo shared

--- src/app/coordinator/coordinator.module.ts
+++ src/app/coordinator/coordinator.module.ts
@@ -3,18 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { CoordinatorRoutingModule } from './coordinator-routing.module';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { SidebarItemsComponent } from './components/sidebar-items/sidebar-items.component';
-import { SidebarItemComponent } from './components/sidebar-item/sidebar-item.component';
 import { StudentsComponent } from './pages/students/students.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SidebarComponent,
-    SidebarItemsComponent,
-    SidebarItemComponent,
     StudentsComponent,
   ],
   imports: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorLabelDirective } from './directives/error-label.directive';
 import { LogoComponent } from './components/logo/logo.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarItemComponent } from './components/sidebar-item/sidebar-item.component';
+import { SidebarItemsComponent } from './components/sidebar-items/sidebar-items.component';
 
 
 @NgModule({
   declarations: [
     ErrorLabelDirective,
-    LogoComponent
+    LogoComponent,
+    SidebarComponent,
+    SidebarItemsComponent,
+    SidebarItemComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ErrorLabelDirective,
-    LogoComponent
+    LogoComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
